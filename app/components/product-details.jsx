4f48ae4e88e6f73085ae9977d02b2e596de529ec
teqsify <teqsify.it@gmail.com>
2024-05-24 12:07:30 +0100

    Updated navigations and Product page built 2

--- app/components/product-details.jsx
+++ app/components/product-details.jsx
@@ -10,7 +10,7 @@ export default function ProductClient() {
     <>
       <section
         id="shop-section"
-        className="w-full py-12 md:py-24 lg:py-32 bg-gradient-to-r from-blue-gray-700 to-blue-gray-900 dark:bg-gray-950">
+        className="w-full mt-10 py-12 md:py-24 lg:py-32 bg-gradient-to-r from-blue-gray-700 to-blue-gray-900 dark:bg-gray-950">
         <div className="container px-4 md:px-6">
           <div className="flex flex-col items-center justify-center space-y-4 text-center">
             <div className="space-y-2">
